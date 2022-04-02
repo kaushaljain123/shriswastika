3cfede6720cb7a45ef6e2bc1d7510ab627fcbbc8
Bringonstore <84062175+myofflinemarket@users.noreply.github.com>
2022-04-02 13:56:45 +0530

    sd

--- frontend/src/screens/OurPolicies.js
+++ frontend/src/screens/OurPolicies.js
@@ -6,25 +6,8 @@ const OurPolicies = () => {
       <Container>
         <Row>
             <Col>
-                <h3>Terms & Conditions</h3>
-                <p>By providing us your Information or by making use of the facilities provided by the Website, You hereby consent to the collection, storage, processing and transfer of any or all of Your Personal Information and Non-Personal Information by us as specified under this Privacy Policy. You further agree that such collection, use, storage and transfer of Your Information shall not cause any loss or wrongful gain to you or any other person.</p>
-                <h3>USER INFORMATION</h3>
-                <p>To avail certain services on our Websites, users are required to provide certain information for the registration process namely: – a) your name, b) email address, c) age, d) PIN code, e) credit card, or debit card details f) password, etc. The Information as supplied by the users enables us to improve our sites and provide you the most user-friendly experience.
-
-                All required information is service dependent and we may use the above said user information to, maintain, protect, and improve its services (including advertising services) and for developing new services such information will not be considered as sensitive if it is freely available and accessible in the public domain or is furnished under the Right to Information Act, 2005 or any other law for the time being in force. 
-                </p>
-                <h3>Cookies</h3>
-                <p>
-                To improve the responsiveness of the sites for our users, we may use “cookies”, or similar electronic tools to collect information to assign each visitor a unique, random number as a User Identification (User ID) to understand the user’s individual interests using the Identified Computer. Unless you voluntarily identify yourself (through registration, for example), we will have no way of knowing who you are, even if we assign a cookie to your computer. The only personal information a cookie can contain is information you supply (an example of this is when you ask for our Personalised Horoscope). A cookie cannot read data off your hard drive. Our advertisers may also assign their own cookies to your browser (if you click on their ads), a process that we do not control.
-
-                Our web servers automatically collect limited information about your computer’s connection to the Internet, including your IP address, when you visit our site. (Your IP address is a number that lets computers attached to the Internet know where to send you data — such as the web pages you view.) Your IP address does not identify you personally. We use this information to deliver our web pages to you upon request, to tailor our site to the interests of our users, to measure traffic within our site and let advertisers know the geographic locations from where our visitors come.
-                </p>
-                <h3>INFORMATION SECURITY</h3>
-                <p>
-                We take appropriate security measures to protect against unauthorized access to or unauthorized alteration, disclosure or destruction of data. These include internal reviews of our data collection, storage and processing practices and security measures, including appropriate encryption and physical security measures to guard against unauthorized access to systems where we store personal data.
-
-                All information gathered on our Website is securely stored within our controlled database. The database is stored on servers secured behind a firewall; access to the servers is password-protected and is strictly limited. However, as effective as our security measures are, no security system is impenetrable. We cannot guarantee the security of our database, nor can we guarantee that information you supply will not be intercepted while being transmitted to us over the Internet. And, of course, any information you include in a posting to the discussion areas is available to anyone with Internet access.
-                </p>
+                <h3>Cancellation/ Return/ Refund Policy</h3>
+                <p>In case of any issue with our service and / or cancellation, With in 7 Days OR please get in touch with us using Contact Form.</p>
             </Col>
         </Row>
     </Container>
